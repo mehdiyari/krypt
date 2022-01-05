8cbefb5e77b3ecdf0f36463a21cc07bfe336662d
Mehdi Yari <mehdi.email.info@gmail.com>
2022-01-05 22:54:37 +0330

    Implement view-logic of CreateAccountFragment.kt

--- app/src/main/java/ir/mehdiyari/krypt/ui/logout/CreateAccountViewModel.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/logout/CreateAccountViewModel.kt
@@ -1,10 +1,55 @@
 package ir.mehdiyari.krypt.ui.logout
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.mehdiyari.krypt.R
+import ir.mehdiyari.krypt.data.repositories.AccountsRepository
+import ir.mehdiyari.krypt.data.repositories.BadAccountNameThrowable
+import ir.mehdiyari.krypt.data.repositories.PasswordLengthThrowable
+import ir.mehdiyari.krypt.di.qualifiers.DispatcherIO
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CreateAccountViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class CreateAccountViewModel @Inject constructor(
+    private val accountsRepository: AccountsRepository,
+    @DispatcherIO private val ioDispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    private val _createAccountViewState: MutableSharedFlow<CreateAccountViewState> =
+        MutableSharedFlow(replay = 0)
+
+    val createAccountViewState: SharedFlow<CreateAccountViewState> = _createAccountViewState
+
+    fun addAccount(name: String, password: String) {
+        viewModelScope.launch(ioDispatcher) {
+            accountsRepository.addAccount(name, password).also { result ->
+                if (result.first) {
+                    _createAccountViewState.emit(CreateAccountViewState.SuccessCreateAccount)
+                } else {
+                    when (result.second) {
+                        is PasswordLengthThrowable -> R.string.password_length_error
+                        is BadAccountNameThrowable -> R.string.account_length_error
+                        else -> R.string.something_went_wrong
+                    }.also {
+                        _createAccountViewState.emit(
+                            CreateAccountViewState.FailureCreateAccount(
+                                it
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+sealed class CreateAccountViewState {
+    object SuccessCreateAccount : CreateAccountViewState()
+    data class FailureCreateAccount(@StringRes val errorResId: Int) : CreateAccountViewState()
 }
