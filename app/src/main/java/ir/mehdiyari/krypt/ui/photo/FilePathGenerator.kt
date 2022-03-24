4898b5275b171d904ac031c35786045e3bd022b4
Mehdi Yari <mehdi.email.info@gmail.com>
2022-03-24 10:46:07 +0430

    Add some utility methods to FilePathGenerator.kt

--- app/src/main/java/ir/mehdiyari/krypt/ui/photo/FilePathGenerator.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/photo/FilePathGenerator.kt
@@ -10,8 +10,24 @@ class FilePathGenerator @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
 
+    companion object {
+        const val CRYPT_FILES_PREFIX = "krypt_"
+    }
+
     fun generateFilePathForPhotos(photoPath: String): String =
-        "${context.cacheDir.path}/krypt_${System.currentTimeMillis()}.${
+        "${context.cacheDir.path}/${CRYPT_FILES_PREFIX}${System.currentTimeMillis()}.${
             photoPath.split(".").lastOrNull() ?: ".jpg"
         }"
+
+    fun generateFilePathForPhotosThumbnail(thumbnailPath: String): String =
+        "${context.cacheDir.path}/${getNameOfFile(thumbnailPath)}.${
+            thumbnailPath.split(".").lastOrNull() ?: ".jpg"
+        }"
+
+    fun getCashDir(): String = context.cacheDir.path
+
+    fun getNameOfFile(path: String): String = path.substring(
+        path.lastIndexOf("/") + 1,
+        path.lastIndexOf(".")
+    )
 }
