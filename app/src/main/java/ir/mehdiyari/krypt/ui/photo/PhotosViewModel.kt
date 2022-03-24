4b080cd5ac7c12783c8ffa767f90c2a6dfa3a53b
Mehdi Yari <mehdi.email.info@gmail.com>
2022-03-24 10:49:58 +0430

    Move MediaStoreManager.kt to utils package

--- app/src/main/java/ir/mehdiyari/krypt/ui/photo/PhotosViewModel.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/photo/PhotosViewModel.kt
@@ -10,6 +10,7 @@ import ir.mehdiyari.krypt.data.repositories.FilesRepository
 import ir.mehdiyari.krypt.di.qualifiers.AccountName
 import ir.mehdiyari.krypt.di.qualifiers.DispatcherIO
 import ir.mehdiyari.krypt.utils.FilePathGenerator
+import ir.mehdiyari.krypt.utils.MediaStoreManager
 import ir.mehdiyari.krypt.utils.ThumbsUtils
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
--- app/src/main/java/ir/mehdiyari/krypt/utils/MediaStoreManager.kt
+++ app/src/main/java/ir/mehdiyari/krypt/utils/MediaStoreManager.kt
@@ -1,4 +1,4 @@
-package ir.mehdiyari.krypt.ui.photo
+package ir.mehdiyari.krypt.utils
 
 import android.content.Context
 import android.media.MediaScannerConnection
