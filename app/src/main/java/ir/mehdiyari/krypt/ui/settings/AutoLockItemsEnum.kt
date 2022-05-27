e9bba0e76fc9f7787e3bf57c1a6ed1ba879381fa
Mehdi Yari <mehdi.email.info@gmail.com>
2022-05-27 15:25:18 +0430

    Add real values in seconds to AutoLockItemsEnum
    Signed-off-by: Mehdi.Yari <mehdi.yari@snapp.cab>

--- app/src/main/java/ir/mehdiyari/krypt/ui/settings/AutoLockItemsEnum.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/settings/AutoLockItemsEnum.kt
@@ -1,13 +1,13 @@
 package ir.mehdiyari.krypt.ui.settings
 
-enum class AutoLockItemsEnum {
-    Disabled,
-    ThirtySecond,
-    OneMinute,
-    TwoMinute,
-    FiveMinute,
-    TenMinute,
-    FifteenMinute,
-    ThirtyMinute,
-    OneHour
+enum class AutoLockItemsEnum(val value: Int) {
+    Disabled(-1),
+    ThirtySecond(30),
+    OneMinute(60),
+    TwoMinute(120),
+    FiveMinute(300),
+    TenMinute(600),
+    FifteenMinute(900),
+    ThirtyMinute(1800),
+    OneHour(3600)
 }
