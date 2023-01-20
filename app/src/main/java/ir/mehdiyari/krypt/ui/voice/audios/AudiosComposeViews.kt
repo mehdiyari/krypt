40a703272da5065f937f36f1a7dbac9244670a2e
Mehdi Yari <mehdi.email.info@gmail.com>
2023-01-20 14:57:00 +0330

    Make UI state-full based on states in AudiosViewModel

--- app/src/main/java/ir/mehdiyari/krypt/ui/voice/audios/AudiosComposeViews.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/voice/audios/AudiosComposeViews.kt
@@ -2,11 +2,15 @@ package ir.mehdiyari.krypt.ui.voice.audios
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -16,15 +20,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import ir.mehdiyari.krypt.R
 import ir.mehdiyari.krypt.utils.KryptTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 @Preview
 fun AudiosScreen(
-    navController: NavController? = null
+    navController: NavController? = null,
+    audiosViewModel: AudiosViewModel = viewModel()
 ) {
     KryptTheme(darkTheme = true) {
         Scaffold(
@@ -43,14 +51,61 @@ fun AudiosScreen(
                 )
             }
         ) {
-
+            AudioList(
+                audiosViewModel.audios.collectAsState(),
+                audiosViewModel.currentAudioPlaying,
+                audiosViewModel::onAudioAction
+            )
         }
     }
 }
 
 @Composable
 @Preview
-fun AudioItem() {
+private fun AudioList(
+    audios: State<List<AudioEntity>> = mutableStateOf(
+        listOf(
+            AudioEntity(1L, "Voice #60", "08:30", "2023/01/26 18:00:00"),
+            AudioEntity(2L, "Voice #61", "23:03", "2023/12/10 19:11:00"),
+            AudioEntity(3L, "Voice #62", "16:30", "2022/12/12 10:00:00")
+        )
+    ),
+    currentAudioPlaying: StateFlow<AudioEntity?> = MutableStateFlow(
+        AudioEntity(
+            1L,
+            "Voice #60",
+            "08:30",
+            "2023/01/26 18:00:00"
+        )
+    ),
+    onActionClicked: (AudioEntity) -> Unit = {}
+) {
+    val playingAudioState = currentAudioPlaying.collectAsState()
+    LazyColumn(content = {
+        items(audios.value.size, key = {
+            audios.value[it].id
+        }) {
+            AudioItem(audios.value[it], playingAudioState, onActionClicked)
+        }
+    })
+}
+
+@Composable
+@Preview
+fun AudioItem(
+    audioEntity: AudioEntity = AudioEntity(
+        1L, "Voice #60", "08:30", "2023/01/26 09:00:00"
+    ),
+    playingAudioState: State<AudioEntity?> = mutableStateOf(
+        AudioEntity(
+            1L,
+            "Voice #60",
+            "08:30",
+            "2023/01/26 18:00:00"
+        )
+    ),
+    onActionClicked: (AudioEntity) -> Unit = {}
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -65,9 +120,16 @@ fun AudioItem() {
                 .padding(4.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = {
+                onActionClicked.invoke(audioEntity)
+            }) {
+                val playPauseIcon = if (playingAudioState.value?.id == audioEntity.id) {
+                    painterResource(id = R.drawable.ic_pause)
+                } else {
+                    painterResource(id = R.drawable.ic_audio_play)
+                }
                 Icon(
-                    painter = painterResource(id = R.drawable.ic_audio_play),
+                    painter = playPauseIcon,
                     contentDescription = ""
                 )
             }
@@ -81,11 +143,11 @@ fun AudioItem() {
                         .padding(start = 4.dp, end = 4.dp)
                         .constrainAs(columnRef) {}
                 ) {
-                    Text(text = "Voice #60", fontWeight = FontWeight.Bold)
-                    Text(text = "01:10:23", fontSize = 12.sp)
+                    Text(text = audioEntity.name, fontWeight = FontWeight.Bold)
+                    Text(text = audioEntity.duration, fontSize = 12.sp)
                 }
 
-                Text(text = "2023/01/01 23:32", fontSize = 10.sp, modifier = Modifier
+                Text(text = audioEntity.dateTime, fontSize = 10.sp, modifier = Modifier
                     .padding(end = 4.dp)
                     .constrainAs(dateText) {
                         end.linkTo(parent.end)
