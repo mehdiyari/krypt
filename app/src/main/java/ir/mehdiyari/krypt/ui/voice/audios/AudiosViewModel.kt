d3af04a63cf9cf52fac8ca349914dd91b6c268ef
Mehdi Yari <mehdi.email.info@gmail.com>
2023-01-20 14:56:09 +0330

    Declare needed states for Audios view in AudiosViewModel.kt

--- app/src/main/java/ir/mehdiyari/krypt/ui/voice/audios/AudiosViewModel.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/voice/audios/AudiosViewModel.kt
@@ -2,9 +2,23 @@ package ir.mehdiyari.krypt.ui.voice.audios
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class AudiosViewModel @Inject constructor(
 
-) : ViewModel()
+) : ViewModel() {
+
+    private val _audios = MutableStateFlow<List<AudioEntity>>(listOf())
+    val audios = _audios.asStateFlow()
+
+    private val _currentAudioPlaying = MutableStateFlow<AudioEntity?>(null)
+    val currentAudioPlaying = _currentAudioPlaying.asStateFlow()
+
+
+    fun onAudioAction(audioEntity: AudioEntity) {
+        TODO()
+    }
+}
