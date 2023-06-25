add3d19b3b65e1866ed2cce73d8a1b28a7983ea4
Mehdi Yari <mehdi.email.info@gmail.com>
2023-06-25 21:36:20 +0200

    Open media picker for encrypt and decrypt states in composable functions based on given states

--- app/src/main/java/ir/mehdiyari/krypt/ui/media/MediaRoute.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/media/MediaRoute.kt
@@ -1,20 +1,32 @@
 package ir.mehdiyari.krypt.ui.media
 
+import android.annotation.SuppressLint
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.fragment.findNavController
+import ir.mehdiyari.fallery.main.fallery.FalleryOptions
+import ir.mehdiyari.fallery.main.fallery.getFalleryActivityResultContract
 import ir.mehdiyari.krypt.R
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun MediaRoute(
     modifier: Modifier = Modifier,
@@ -26,6 +38,7 @@ fun MediaRoute(
     var notifyMediaScanner by remember { mutableStateOf(true) }
 
     MediaScreen(
+        modifier = modifier,
         actionState = actionState,
         viewState = viewState,
         notifyMediaScanner = notifyMediaScanner,
@@ -38,12 +51,18 @@ fun MediaRoute(
                 encrypted
             )
         },
-        modifier = modifier
+        defaultFalleryOptions = viewModel.getDefaultFalleryOptions(),
+        kryptFalleryOptions = viewModel.getKryptFalleryOptions(),
+        onBackPressed = onBackPressed,
+        onSelectMedia = viewModel::onSelectedMedias,
+        checkForOpenPickerForDecryptMode = viewModel::checkForOpenPickerForDecryptMode
     )
 }
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun MediaScreen(
+    modifier: Modifier,
     actionState: MediaFragmentAction,
     viewState: MediaViewState,
     notifyMediaScanner: Boolean,
@@ -51,12 +70,41 @@ fun MediaScreen(
     deleteAllSelectedFiles: () -> Unit,
     removeItemFromList: (String) -> Unit,
     deleteSelectedFromList: (String, Boolean) -> Unit,
-    modifier: Modifier
+    defaultFalleryOptions: FalleryOptions,
+    kryptFalleryOptions: FalleryOptions,
+    onBackPressed: () -> Unit,
+    onSelectMedia: (List<String>) -> Unit,
+    checkForOpenPickerForDecryptMode: () -> Boolean,
 ) {
-
-    Box(modifier = modifier) {
+    val isFalleryOpenedBefore = remember { mutableStateOf(false) }
+    val falleryLauncher = rememberLauncherForActivityResult(getFalleryActivityResultContract()) {
+        if (actionState == MediaFragmentAction.DECRYPT_MEDIA && it.mediaPathList.isNullOrEmpty()) {
+            onBackPressed()
+            return@rememberLauncherForActivityResult
+        }
+        onSelectMedia(it.mediaPathList ?: listOf())
+    }
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
         when (viewState) {
-            MediaViewState.Default -> CircularProgressIndicator()
+            MediaViewState.Default -> {
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .size(60.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                }
+            }
+
             MediaViewState.OperationStart -> OperationStart()
             MediaViewState.OperationFinished -> {
                 val textRes = when (actionState) {
@@ -88,13 +136,39 @@ fun MediaScreen(
 
         }
 
+        ShowActionButton(
+            viewState = viewState,
+            actionState = actionState,
+            notifyMediaScanner = notifyMediaScanner,
+            deleteAllSelectedFiles = deleteAllSelectedFiles,
+            modifier = modifier,
+        )
     }
 
 
-    ShowActionButton(
-        viewState = viewState,
-        actionState = actionState,
-        notifyMediaScanner = notifyMediaScanner,
-        deleteAllSelectedFiles = deleteAllSelectedFiles
-    )
+    if (viewState == MediaViewState.Default) {
+        if (!isFalleryOpenedBefore.value) {
+            if (actionState == MediaFragmentAction.PICK_MEDIA) {
+                SideEffect {
+                    falleryLauncher.launch(defaultFalleryOptions)
+                    isFalleryOpenedBefore.value = true
+                }
+            } else if (actionState == MediaFragmentAction.DECRYPT_MEDIA) {
+                val context = LocalContext.current
+                if (!checkForOpenPickerForDecryptMode()) {
+                    Toast.makeText(
+                        context,
+                        R.string.no_encrypted_file_found,
+                        Toast.LENGTH_LONG
+                    ).show()
+                    onBackPressed()
+                } else {
+                    SideEffect {
+                        falleryLauncher.launch(kryptFalleryOptions)
+                        isFalleryOpenedBefore.value = true
+                    }
+                }
+            }
+        }
+    }
 }
