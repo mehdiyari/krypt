1c19b13fa4bee9433c6a51808d66bc8b8c52a7ef
Mehdi Yari <mehdi.email.info@gmail.com>
2023-07-02 16:32:08 +0200

    Add three interface for manager and also get the username and key of current user

--- app/src/main/java/ir/mehdiyari/krypt/app/KryptApplication.kt
+++ app/src/main/java/ir/mehdiyari/krypt/app/KryptApplication.kt
@@ -3,15 +3,39 @@ package ir.mehdiyari.krypt.app
 import android.app.Application
 import com.jakewharton.processphoenix.ProcessPhoenix
 import dagger.hilt.android.HiltAndroidApp
+import ir.mehdiyari.krypt.app.user.CurrentUserManager
+import ir.mehdiyari.krypt.app.user.UserKeyProvider
+import ir.mehdiyari.krypt.app.user.UsernameProvider
+import ir.mehdiyari.krypt.data.repositories.CurrentUser
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 @HiltAndroidApp
-class KryptApplication : Application() {
+class KryptApplication : Application(), UsernameProvider, UserKeyProvider, CurrentUserManager {
+
+    private val currentUser = CurrentUser(accountName = null, key = null)
 
     override fun onCreate() {
         super.onCreate()
         if (ProcessPhoenix.isPhoenixProcess(this)) {
             return
         }
+    }
+
+    override fun getUsername(): String? = currentUser.accountName
+
+    override fun getKey(): SecretKey? = currentUser.key?.let {
+        SecretKeySpec(it, "AES")
+    }
+
+    override fun setCurrentUser(username: String, key: ByteArray) {
+        currentUser.clear()
+        currentUser.accountName = username
+        currentUser.key = key
+    }
+
+    override fun clearCurrentUser() {
+        currentUser.clear()
     }
 
 }
---
+++ app/src/main/java/ir/mehdiyari/krypt/app/user/UsernameProvider.kt
@@ -0,0 +1,7 @@
+package ir.mehdiyari.krypt.app.user
+
+interface UsernameProvider {
+
+    fun getUsername(): String?
+
+}
