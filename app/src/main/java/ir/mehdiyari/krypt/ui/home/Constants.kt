ee621c73e5013096804c83b04f2f3b9c408bfb60
Mehdi Yari <mehdi.email.info@gmail.com>
2023-07-03 10:29:21 +0200

    Remove delete temporary in the main menu items

--- app/src/main/java/ir/mehdiyari/krypt/ui/home/Constants.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/home/Constants.kt
@@ -11,5 +11,4 @@ val ADD_ITEMS = listOf(
 val MAIN_MENU_ITEMS = listOf(
     R.drawable.ic_data_usage_24 to R.string.menu_data_usage,
     R.drawable.ic_settings_24 to R.string.menu_settings,
-    R.drawable.ic_help_24 to R.string.menu_help,
 )
