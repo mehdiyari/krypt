14ccf0f4de78443a6a919bf4b7da01a24fadc88d
MHD <m.dabbaghy@gmail.com>
2023-07-04 23:20:19 +0330

    - Optimize imports in AudiosListComponents.kt

--- app/src/main/java/ir/mehdiyari/krypt/ui/voice/audios/AudiosListComponents.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/voice/audios/AudiosListComponents.kt
@@ -33,7 +33,6 @@ import ir.mehdiyari.krypt.R
 import ir.mehdiyari.krypt.ui.voice.audios.entity.AudioEntity
 import ir.mehdiyari.krypt.ui.voice.player.MusicPlayerEntity
 import ir.mehdiyari.krypt.utils.KryptTheme
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
