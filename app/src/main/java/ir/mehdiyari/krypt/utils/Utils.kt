4f66f16cea5eb47e872a338a7f2a5b50512495d2
Mehdi Yari <mehdi.email.info@gmail.com>
2023-07-05 21:45:13 +0200

    Move isInDarkTheme to Theme.kt

--- app/src/main/java/ir/mehdiyari/krypt/utils/Utils.kt
+++ app/src/main/java/ir/mehdiyari/krypt/utils/Utils.kt
@@ -1,13 +1,6 @@
 package ir.mehdiyari.krypt.utils
 
-import android.content.Context
-import android.content.res.Configuration
-
 fun getFileProviderAuthority(packageName: String): String = "${packageName}.provider"
-
-fun Context.isInDarkTheme(): Boolean {
-    return (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
-}
 
 fun formatSize(size: Long): String {
     var internalSize = size
