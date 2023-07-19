ad3cb421a0cf03df09b77b143aef94f5dfe3a5f2
MHD <m.dabbaghy@gmail.com>
2023-07-19 17:54:27 +0330

    Some cleanup in applying plugin and extensions

--- build-logic/convention/src/main/kotlin/AndroidLibraryComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryComposeConventionPlugin.kt
@@ -1,17 +1,16 @@
+import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.LibraryExtension
 import ir.mehdiyari.krypt.applyAndroidLibraryPlugin
 import ir.mehdiyari.krypt.configureComposeAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidLibraryComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            applyAndroidLibraryPlugin()
-            extensions.configure<LibraryExtension> {
-                configureComposeAndroid(this)
-            }
+            configureComposeAndroid(extensions.getByType<LibraryExtension>())
         }
     }
 }
