e9f063082931418275ef98365bf7ec9d7e7f77bd
MHD <m.dabbaghy@gmail.com>
2023-07-19 17:54:27 +0330

    Some cleanup in applying plugin and extensions

--- build-logic/convention/src/main/kotlin/AndroidApplicationFlavorConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFlavorConventionPlugin.kt
@@ -1,16 +1,16 @@
 import com.android.build.api.dsl.ApplicationExtension
+import ir.mehdiyari.krypt.applyAndroidApplicationPlugin
 import ir.mehdiyari.krypt.configureFlavor
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationFlavorConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
         with(target){
-            extensions.configure<ApplicationExtension> {
-                configureFlavor(this)
-            }
-
+            applyAndroidApplicationPlugin()
+            configureFlavor(extensions.getByType<ApplicationExtension>())
         }
     }
 }
