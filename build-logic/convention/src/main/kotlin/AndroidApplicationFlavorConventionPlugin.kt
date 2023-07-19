42e67f2b14d8cfad2952524e57c126182af7e180
MHD <m.dabbaghy@gmail.com>
2023-07-19 17:54:44 +0330

    Optimize import

--- build-logic/convention/src/main/kotlin/AndroidApplicationFlavorConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFlavorConventionPlugin.kt
@@ -3,7 +3,6 @@ import ir.mehdiyari.krypt.applyAndroidApplicationPlugin
 import ir.mehdiyari.krypt.configureFlavor
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationFlavorConventionPlugin: Plugin<Project> {
