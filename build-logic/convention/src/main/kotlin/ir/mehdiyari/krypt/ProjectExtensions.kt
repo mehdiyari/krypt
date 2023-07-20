c0f13d6f1ee1798e8a8e00f457befe86a5d10dd3
MHD <m.dabbaghy@gmail.com>
2023-07-20 10:14:39 +0330

    Add comment in ProjectExtensions.kt

--- build-logic/convention/src/main/kotlin/ir/mehdiyari/krypt/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/ir/mehdiyari/krypt/ProjectExtensions.kt
@@ -8,6 +8,9 @@ import org.gradle.kotlin.dsl.getByType
 val Project.libs
     get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
+/*Currently it is the best way to add plugin from VersionCatalog, I need to find a better way later
+* Other approach is using plugin id directly
+*/
 fun Project.applyAndroidApplicationPlugin(){
     pluginManager.apply(libs.findPlugin("android.application").get().get().pluginId)
 }
