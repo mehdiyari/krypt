825b88ede26889b03055e871a54e584ad4387d5a
Mehdi Yari <mehdi.email.info@gmail.com>
2023-07-24 22:15:17 +0200

    Add decode and encodeUrlSafe methods to Base64Wrapper

--- app/src/main/java/ir/mehdiyari/krypt/data/repositories/Base64Wrapper.kt
+++ app/src/main/java/ir/mehdiyari/krypt/data/repositories/Base64Wrapper.kt
@@ -7,12 +7,17 @@ import ir.mehdiyari.krypt.cryptography.utils.getBytesBetweenIndexes
 import javax.inject.Inject
 
 class Base64Wrapper @Inject constructor() {
-    fun decode(encryptedName: String): ByteArray =
-        Base64.decode(encryptedName)
+
+    fun decode(base64String: String): ByteArray =
+        Base64.decode(base64String)
             .let { name ->
                 name.getBytesBetweenIndexes(
                     start = name.size - (SymmetricHelper.INITIALIZE_VECTOR_SIZE + HashingUtils.SALT_SIZE),
                     end = name.size - SymmetricHelper.INITIALIZE_VECTOR_SIZE
                 )
             }
+
+    fun encode(byteArray: ByteArray): String = Base64.encodeBytes(byteArray)
+
+    fun encodeUrlSafe(byteArray: ByteArray): String = Base64.encodeBytes(byteArray, Base64.URL_SAFE)
 }
