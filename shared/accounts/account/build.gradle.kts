3c36be2da3fdbd2f6c3e5beb0d8a98a15164498a
MHD <m.dabbaghy@gmail.com>
2023-07-24 09:33:52 +0330

    Complete migration build.gradle account module

--- shared/accounts/account/build.gradle.kts
+++ shared/accounts/account/build.gradle.kts
@@ -1,22 +1,22 @@
 plugins {
-    id 'krypt.android.library'
-    id 'krypt.android.hilt'
+    id("krypt.android.library")
+    id("krypt.android.hilt")
 }
 
 android {
-    namespace 'ir.mehdiyari.krypt.account'
+    namespace = "ir.mehdiyari.krypt.account"
 
     defaultConfig {
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 }
 
 dependencies {
-    implementation libs.room
-    implementation libs.coreKtx
-    testImplementation libs.junit
-    androidTestImplementation libs.testExt
+    implementation(libs.room)
+    implementation(libs.coreKtx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.testExt)
 
-    implementation project(":shared:cryptography")
-    implementation project(":shared:accounts:account-data")
+    implementation(project(":shared:cryptography"))
+    implementation(project(":shared:accounts:account-data"))
 }
