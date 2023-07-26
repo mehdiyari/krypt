847514c6de5a31533ba477b68d256863a90bf308
Mehdi Yari <mehdi.email.info@gmail.com>
2023-07-26 23:58:54 +0200

    Add settings module

---
+++ features/settings/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("krypt.android.library")
+    id("krypt.android.library.compose")
+    id("krypt.android.hilt")
+}
+
+android {
+    namespace = "ir.mehdiyari.krypt.setting"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation(project(":shared:designsystem:theme"))
+
+    implementation(project(":shared:dispatchers"))
+    implementation(project(":shared:accounts:account"))
+
+    implementation(libs.androidx.lifecycle.viewmodelCompose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    testImplementation(libs.mockk)
+    testImplementation(libs.coroutinesTest)
+    testImplementation(libs.junit)
+
+}
