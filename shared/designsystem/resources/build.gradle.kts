e42f7f56fc2c39ada77599193eb8d034350db724
MHD <m.dabbaghy@gmail.com>
2023-07-26 13:22:44 +0330

    Add resource module

---
+++ shared/designsystem/resources/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("krypt.android.library")
+}
+
+android {
+    namespace = "ir.mehdiyari.kryot.shared.designsystem.resources"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+}
+
+dependencies {
+
+}
