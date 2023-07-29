644ae515a820b47fc949da367da9a5c5d32bb367
MHD <m.dabbaghy@gmail.com>
2023-07-29 20:11:21 +0330

    Create text logic module

---
+++ features/text/logic/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("krypt.android.library")
+    id("krypt.android.hilt")
+}
+
+android {
+    namespace = "ir.mehdiyari.krypt.textLogic"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+
+}
