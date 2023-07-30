160a114010bce83f551fb79ac0a6e6d72dc59dbe
MHD <m.dabbaghy@gmail.com>
2023-07-30 17:07:01 +0330

    Apply feature plugin

--- features/text/list/build.gradle.kts
+++ features/text/list/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    id("krypt.android.library")
-    id("krypt.android.library.compose")
-    id("krypt.android.hilt")
+    id("krypt.android.feature")
 }
 
 android {
@@ -14,15 +12,10 @@ android {
 
 dependencies {
 
-    implementation(project(":shared:designsystem:theme"))
     implementation(project(":shared:designsystem:resources"))
 
-    implementation(project(":shared:dispatchers"))
     implementation(project(":shared:files:files-logic"))
     implementation(project(":shared:files:files-data"))
-    implementation(project(":features:text:shared"))
 
-    implementation(libs.androidx.lifecycle.viewmodelCompose)
-    implementation(libs.androidx.lifecycle.runtimeCompose)
-    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(project(":features:text:shared"))
 }
