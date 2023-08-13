29dd02b7efb218280cfc62999c3c8ef7e79027d5
MHD <m.dabbaghy@gmail.com>
2023-08-13 11:23:43 +0330

    Merge with dev and fix conflicts

--- app/src/main/java/ir/mehdiyari/krypt/ui/KryptApp.kt
+++ app/src/main/java/ir/mehdiyari/krypt/ui/KryptApp.kt
@@ -23,8 +23,6 @@ import ir.mehdiyari.krypt.features.auth.login.ROUTE_LOGIN
 import ir.mehdiyari.krypt.features.text.add.navigateToAddText
 import ir.mehdiyari.krypt.setting.ui.navigateToSettings
 import ir.mehdiyari.krypt.shareContent.ShareDataViewModel
-import ir.mehdiyari.krypt.ui.media.MediaViewAction
-import ir.mehdiyari.krypt.ui.media.navigateToMedia
 import ir.mehdiyari.krypt.mediaList.MediaViewAction
 import ir.mehdiyari.krypt.mediaList.navigateToMedia
 import ir.mehdiyari.krypt.ui.navigation.KryptNaveHost
