cb376b0bad3ae7c0091e277d5bbdeb6090715f4e
ghaseminia <akram.ghaseminia@snapp.cab>
2023-09-15 16:08:57 +0330

    - Remove extra dependencies and proguard files

--- shared/testing/build.gradle.kts
+++ shared/testing/build.gradle.kts
@@ -12,10 +12,6 @@ android {
 
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-
     api(libs.mockk)
     api(libs.coroutinesTest)
     api(libs.turbine)
