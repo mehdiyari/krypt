60bf67108e0f56fa6cfc214b0ddcb0997d41eb73
Mehdi Yari <mehdi.email.info@gmail.com>
2024-01-28 19:31:15 +0100

    Increase the compile sdk version to 34

--- build-logic/convention/src/main/kotlin/ir/mehdiyari/krypt/Versions.kt
+++ build-logic/convention/src/main/kotlin/ir/mehdiyari/krypt/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val VERSION_NAME = "3.0.2-alpha"
 
     const val MIN_SDK = 23
-    const val COMPILE_SDK = 33
-    const val TARGET_SDK = 33
+    const val COMPILE_SDK = 34
+    const val TARGET_SDK = 34
 
 }
