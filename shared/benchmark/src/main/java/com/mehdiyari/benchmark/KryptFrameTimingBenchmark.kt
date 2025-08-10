0a14fe17e1f850b751374366a3091cfd79cdffdb
Mehdi Yari <mehdi.email.info@gmail.com>
2025-08-10 17:49:28 +0200

    Add KryptFrameTimingBenchmark

--- shared/benchmark/src/main/java/com/mehdiyari/benchmark/KryptFrameTimingBenchmark.kt
+++ shared/benchmark/src/main/java/com/mehdiyari/benchmark/KryptFrameTimingBenchmark.kt
@@ -1,7 +1,7 @@
 package com.mehdiyari.benchmark
 
+import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.StartupMode
-import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -9,7 +9,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class KryptStartupBenchmark {
+class KryptFrameTimingBenchmark {
 
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
@@ -17,12 +17,11 @@ class KryptStartupBenchmark {
     @Test
     fun startup() = benchmarkRule.measureRepeated(
         packageName = "ir.mehdiyari.krypt.dev",
-        metrics = listOf(StartupTimingMetric()),
+        metrics = listOf(FrameTimingMetric()),
         iterations = 5,
-        startupMode = StartupMode.COLD
+        startupMode = StartupMode.COLD,
     ) {
         pressHome()
         startActivityAndWait()
     }
 }
-
